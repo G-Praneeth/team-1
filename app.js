14fd6bbc2514efde02b30af33c2ae2d00cf69e9c
G-Praneeth <113368012+G-Praneeth@users.noreply.github.com>
2022-09-13 20:18:26 +0530

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-const app = require("express")();
-const PORT = process.env.PORT || 2000;
-app.get("", (req, res) => {
-    res.send("Hello world");
-});
-
-app.listen(PORT, () => {
-    console.log('App up at port ${PORT}');
-});
